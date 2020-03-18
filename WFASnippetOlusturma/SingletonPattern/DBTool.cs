60558b612bcad6ca55bcbbacd7a87b54a9553cb6
enes baba <babaoglu_enes@outlook.com>
2020-03-18 16:32:44 +0300

    Add project files.

---
+++ WFASnippetOlusturma/SingletonPattern/DBTool.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WFASnippetOlusturma.SingletonPattern
+{
+    using Models;
+    public class DBTool
+    {
+
+
+
+
+
+
+
+
+
+
+       
+        private static NorthwindEntities _dbInstance;
+
+         public static NorthwindEntities DBInstance()
+        {
+            if (_dbInstance==null)
+            {
+                _dbInstance = new NorthwindEntities();
+            }
+            return _dbInstance;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
